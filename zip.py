1500d3344a3842c6bbc4d42e6727a9d5874b3e45
kbrooks <kbrooks@bravurasolutions.com>
2020-10-14 16:44:47 +1300

    Fix zip.py so temp_file can close allowing it to work with a Windows OS

--- zip.py
+++ zip.py
@@ -30,61 +30,64 @@ for root, dirs, files in os.walk(source_dir):
             if fnmatch.fnmatch(relative_path, pattern):
                 search_results.append(relative_path)
 
-temp_file = tempfile.NamedTemporaryFile(delete=False)
+temp_file_name = None
 try:
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file_name = temp_file.name
 
-    with zipfile.ZipFile(temp_file.name, "w", zipfile.ZIP_DEFLATED) as zip_file:
-        for relative_path, absolute_path in sorted(source_files.items()):
+        with zipfile.ZipFile(temp_file, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            for relative_path, absolute_path in sorted(source_files.items()):
 
-            # Get file/directory details.
-            st = os.stat(absolute_path)
-            is_dir = stat.S_ISDIR(st.st_mode)
-            if is_dir:
-                relative_path += "/"
+                # Get file/directory details.
+                st = os.stat(absolute_path)
+                is_dir = stat.S_ISDIR(st.st_mode)
+                if is_dir:
+                    relative_path += "/"
 
-            info = zipfile.ZipInfo(relative_path)
+                info = zipfile.ZipInfo(relative_path)
 
-            # Set all dates the same.
-            date_time = (1980, 1, 1, 0, 0, 0)
-            info.date_time = date_time
+                # Set all dates the same.
+                date_time = (1980, 1, 1, 0, 0, 0)
+                info.date_time = date_time
 
-            # Set permissions to 755 or 644.
-            if st.st_mode & stat.S_IXUSR:
-                mode = 0o755
+                # Set permissions to 755 or 644.
+                if st.st_mode & stat.S_IXUSR:
+                    mode = 0o755
+                else:
+                    mode = 0o644
+                info.external_attr = (stat.S_IFREG | mode) << 16
+
+                # Read the file contents.
+                if is_dir:
+                    contents = b""
+                    size = 0
+                else:
+                    with open(absolute_path, "rb") as open_file:
+                        contents = open_file.read()
+                    size = len(contents)
+
+                # Add the file to the zip.
+                zip_file.writestr(info, contents)
+
+        output_md5 = hashlib.md5()
+        output_sha = hashlib.sha1()
+        output_sha256 = hashlib.sha256()
+
+        temp_file.seek(0)
+        while True:
+            data = temp_file.read(65536)  # 64kb
+            if data:
+                output_md5.update(data)
+                output_sha.update(data)
+                output_sha256.update(data)
             else:
-                mode = 0o644
-            info.external_attr = (stat.S_IFREG | mode) << 16
+                break
 
-            # Read the file contents.
-            if is_dir:
-                contents = b""
-                size = 0
-            else:
-                with open(absolute_path, "rb") as open_file:
-                    contents = open_file.read()
-                size = len(contents)
-
-            # Add the file to the zip.
-            zip_file.writestr(info, contents)
-
-    output_md5 = hashlib.md5()
-    output_sha = hashlib.sha1()
-    output_sha256 = hashlib.sha256()
-
-    temp_file.seek(0)
-    while True:
-        data = temp_file.read(65536)  # 64kb
-        if data:
-            output_md5.update(data)
-            output_sha.update(data)
-            output_sha256.update(data)
-        else:
-            break
-
-    shutil.move(temp_file.name, output_path)
+    shutil.move(temp_file_name, output_path)
 
 except Exception:
-    os.remove(temp_file.name)
+    if temp_file_name:
+        os.remove(temp_file_name)
     raise
 
 json.dump(
